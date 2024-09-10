796af0d6c231a7cb5089750d346ea7bce6a31061
111 <barnazir@gmail.com>
2024-09-10 21:20:43 +0300

    second com on steak

--- hw/hw/Program.cs
+++ hw/hw/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Я люблю программировать !");
 Console.WriteLine("Я люблю читать произведения Джека Лондона");
+Console.WriteLine("Я люблю ботать !");
