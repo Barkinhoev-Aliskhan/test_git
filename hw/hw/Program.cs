a2b6521631853a199bf12e3564383212929a084c
111 <barnazir@gmail.com>
2024-09-10 21:19:28 +0300

    first com on steak

--- hw/hw/Program.cs
+++ hw/hw/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Я люблю программировать !");
+Console.WriteLine("Я люблю читать произведения Джека Лондона");
